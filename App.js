67db8d7418db591fd90a67631109d5a9d10761fe
Tanvi <tanvibalsara18@gmail.com>
2023-12-30 10:36:00 +0530

    Learning react

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{id:"heading"},"Hello1 world");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+root.render(heading);
+
